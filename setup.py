2fc8377d0b11bd2ee067e2cddae8701201632428
Ulrich Dohou <dohouulrich@gmail.com>
2024-05-21 23:00:36 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 with open("requirements.txt") as f:
-    requirements = f.read().splitlines()
+    requirements = [line.strip() for line in f if line.strip() and not line.startswith("#")]
 
 setup(
     name="dg_util",
